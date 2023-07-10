57ed29a6ee2d42a5d3d281ba2f17991dfa364456
Maulana <maulana.120100@gmail.com>
2023-07-10 09:33:26 +0700

    nambahin selector tim

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { ThesportdbService } from './../thesportdb.service';
 import { Component } from '@angular/core';
-import { Root, Equipment } from './equipment';
+import { Equipment } from './equipment';
+import { Root, Table } from '../tab1/pl';
 
 @Component({
   selector: 'app-tab3',
@@ -8,14 +10,35 @@ import { Root, Equipment } from './equipment';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  selectedTeam: number = 133613;
+  public pl2024Data: Table[] | undefined;
+  apiUrl: string =
+    'https://www.thesportsdb.com/api/v1/json/3/lookupequipment.php?id=';
   public equipmentData: Equipment[] | undefined;
-  constructor(private theSportsDBService: ThesportdbService) {}
+  constructor(
+    private theSportsDBService: ThesportdbService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    this.theSportsDBService.jersey().subscribe((result) => {
+    // this.theSportsDBService.jersey().subscribe((result) => {
+    //   const data: any = result;
+    // this.equipmentData = data.equipment;
+    //   console.log(result);
+    // });
+    this.theSportsDBService.pl2024().subscribe((result: any) => {
+      const data: Root = result;
+      this.pl2024Data = data.table;
+      this.fetchData();
+    });
+    // this.fetchData();
+  }
+  fetchData() {
+    const url = this.apiUrl + this.selectedTeam;
+    this.http.get(url).subscribe((result) => {
       const data: any = result;
       this.equipmentData = data.equipment;
-      console.log(result);
+      // console.log(data);
     });
   }
 }
